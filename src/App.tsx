aa4907fa7900d41c9292eaf5886ecc61c2c46f30
AchrafGroiez <41470d0a@gmail.com>
2024-02-11 02:10:00 -0500

    Changed file position

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Signup from './signup';
+import Signup from './component/page/signup';
 
 export default function App() {
   return (
