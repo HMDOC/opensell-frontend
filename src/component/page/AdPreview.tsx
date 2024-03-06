806acf255e691cc65bf0cba81f265c63d308b9e3
Messicat <112326931+JF195@users.noreply.github.com>
2024-03-05 19:33:25 -0500

    Fixed Weird bugs in AdPrevie

--- src/component/page/AdPreview.tsx
+++ src/component/page/AdPreview.tsx
@@ -1,16 +1,22 @@
 import { ReactElement } from "react";
+import { useNavigate } from "react-router-dom";
 import "../../css/component/page/AdPreview.css"
 
 const AdPreview = (props) : ReactElement => {
 
     //const query = useRef();
+    const adNav = useNavigate();
+
+    const gotoAd = () => {
+        adNav(`/ad/${props?.link}`);
+    }
 
     return (
-        <div id={props?.link} className="adPreview" >
+        <div id={props?.link} className="adPreview" onClick={gotoAd} >
             <img className="card-img-top" src={props?.firstImagePath} alt="The image cant load!"></img>
-            <h3 className="adTxt">{props?.title}</h3>
-            <h4 className="adTxt">{props?.isSold ? "sold" : props?.price+"$"}</h4>
-            <h5 className="adTxt">{props?.shape}</h5>
+            <h3 className="adPreviewText">{props?.title}</h3>
+            <h4 className="adPreviewText">{props?.isSold ? "sold" : props?.price+"$"}</h4>
+            <h5 className="adPreviewText">{props?.shape}</h5>
         </div>
     )
 }
