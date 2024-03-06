ffa8004fab5af2462743bb75a75e3bdc93663672
Messicat <112326931+JF195@users.noreply.github.com>
2024-03-05 19:37:02 -0500

    Refactored SearchBar component

--- src/component/page/SearchBar.tsx
+++ src/component/page/SearchBar.tsx
@@ -1,14 +1,16 @@
-import { Component, ReactNode } from "react";
+import { Component, ReactNode, ReactElement } from "react";
 import "../../css/component/page/SearchBar.css";
 
-export default class SearchBar extends Component {
-    public render(): ReactNode {
-        return(
-            <div id="searchBar">
-                <input placeholder="what are you looking for?" />
-                <button>Search</button>
-            </div>
-        );
-    }
+const SearchBar = (props) : ReactElement => {
 
+    //const query = useRef();
+
+    return (
+        <div id="searchBar">
+            <input placeholder="what are you looking for?" />
+            <button>Search</button>
+        </div>
+    )
 }
+
+export default SearchBar;
