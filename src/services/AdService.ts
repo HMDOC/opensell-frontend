f28ae34e37dc73bb7b61a7fdf7c043c9a200839a
Davide Fuoco <2236970@collegemv.qc.ca>
2024-03-07 09:26:22 -0500

    Started to implement filter search

--- src/services/AdService.ts
+++ src/services/AdService.ts
@@ -6,7 +6,17 @@ import axios from "axios";
 export const getAdByLink = async (link: string) => {
     return await http.get<AdBuyerView>(`/ad/get-ad-buyer-view/${link}`);
 };
-
+/*
+defaultValue = "" String query,
+defaultValue = 0 Double priceMin,
+defaultValue = 9999999d Double priceMax,
+defaultValue = 2020-01-01 Date dateMin,
+defaultValue = "3000-01-01") Date dateMax,
+defaultValue = null Integer typeId, 
+defaultValue = null Set<Integer> tagListId,
+defaultValue = null Integer shapeId,
+defaultValue = "addedDate" String sortBy
+*/
 export const getAdBySearch = async (query: string) => {
     return await http.get<AdSearchPreview[]>(`/ad/search`, {params : { query } })
 }
