d94a68e69cc2b7db051dc4a854aa5c6c92af54c2
Davide Fuoco <2236970@collegemv.qc.ca>
2024-03-07 09:08:08 -0500

    Search bar works now

--- src/component/page/Catalog.tsx
+++ src/component/page/Catalog.tsx
@@ -12,24 +12,26 @@ const ResultList = () : ReactElement => {
 
     //const query = useRef();
     const [listOfAds, setListOfAds] = useState<AdSearchPreview[]>( [] );
-    const searchBarRef = useRef("lor");
+    const searchBarRef = useRef<HTMLInputElement>();
+    const [searchClick, setSearchClick] = useState<boolean>(false);
 
     useEffect(() => {
-        
-        getAdBySearch(searchBarRef.current).then(res => {
+        console.log(searchBarRef);
+        console.log(searchBarRef.current);
+        getAdBySearch(searchBarRef.current.value).then(res => {
             if (res?.data){
                 setListOfAds(res.data);
             }
         })
         
        console.log(listOfAds);
-    }, []);
+    }, [searchClick]);
     // <SearchBar ref={searchBarRef}/>
     return (
         <>
             <div id="searchTop">
                 <h1>Catalog</h1>
-                <SearchBar />
+                <SearchBar reference={searchBarRef} click={setSearchClick}/>
             </div>
             
             <div id="searchResult">
--- src/component/page/SearchBar.tsx
+++ src/component/page/SearchBar.tsx
@@ -4,11 +4,12 @@ import "../../css/component/page/SearchBar.css";
 const SearchBar = (props) : ReactElement => {
 
     //const query = useRef();
-
+    console.log("ts");
+    console.log(props)
     return (
         <div id="searchBar">
-            <input placeholder="what are you looking for?" />
-            <button>Search</button>
+            <input ref={props.reference} placeholder="what are you looking for?" />
+            <button onClick={props.click}>Search</button>
         </div>
     )
 }
