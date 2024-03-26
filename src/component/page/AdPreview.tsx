d6aceb0f35332eb179acbc9e1edc9b674e6161e1
Davide Fuoco <2236970@collegemv.qc.ca>
2024-03-26 16:50:44 -0400

    put icons for clarity

--- src/component/page/AdPreview.tsx
+++ src/component/page/AdPreview.tsx
@@ -1,5 +1,7 @@
 import { ReactElement } from "react";
 import "../../css/component/page/AdPreview.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBox, faBoxOpen, faDizzy, faEarthAmerica, faFaceGrin, faFaceMeh, faFaceSmile, faFaceSmileBeam, faFrown, faGrinStars, faQuestion, faStar, faStarHalf, faStarHalfStroke, faWandMagic, faWandMagicSparkles } from "@fortawesome/free-solid-svg-icons";
 
 /** 
     The preview component for the Ads. Clicking on it will
@@ -7,7 +9,35 @@ import "../../css/component/page/AdPreview.css"
 */
 const AdPreview = (props) : ReactElement => {
 
-    //const query = useRef();
+    /*
+    const shapeIconList = [ 
+        faWandMagicSparkles, 
+        faBoxOpen,
+        faStar,
+        faStarHalfStroke,
+        ,
+        faQuestion
+    ];
+    */
+
+    const shapeIconList = [ 
+        faGrinStars,
+        faFaceSmileBeam, 
+        faFaceSmile,
+        faFaceMeh,
+        faFrown,
+        faDizzy
+    ];
+
+    const shapeTextList = [
+        "Brand New", 
+        "Opened", 
+        "Barely Used", 
+        "Used", 
+        "Bad", 
+        "Unknown"
+    ]
+
     const gotoAd = () => {
         //adNav(`/ad/${props?.link}`, {state : 0});
         window.open(`/ad/${props?.link}`, "_blank", "noreferrer");
@@ -17,12 +47,22 @@ const AdPreview = (props) : ReactElement => {
         return (props?.isSold ? style : "");
     }
 
+    const getShapeIcon = (shape:number) => {
+        return (
+            <FontAwesomeIcon icon={shapeIconList[shape]}/>
+        )
+    }
+
+    const getShapeName = (shape:number) => {
+        return shapeTextList[shape]
+    }
+
     return (
         <div id={props?.link} className={"adPreview " + addSoldCSS("adSold")} onClick={gotoAd} >
             <img className="card-img-top" src={props?.firstImagePath} alt="The image cant load!"></img>
             <h3 className="adPreviewText"> {props?.title}</h3>
             <h4 className={"adPreviewText " + addSoldCSS("adSoldText")}>{props?.isSold ? "sold" : props?.price+"$"}</h4>
-            <h5 className="adPreviewText">{props?.shape}</h5>
+            <h5 className="adPreviewText" title={getShapeName(props?.shape)}> {getShapeIcon(props?.shape)}</h5>
         </div>
     )
 }
