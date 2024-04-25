48e3f60844d57945aaa6ca43b61f9596b0b5861b
Messicat <112326931+JF195@users.noreply.github.com>
2024-04-24 21:25:06 -0400

    Updated CSS of Catalog

--- src/component/part/AdView/AdPricePart.tsx
+++ src/component/part/AdView/AdPricePart.tsx
@@ -3,7 +3,7 @@ import "../../../css/component/part/AdView/AdPricePart.scss";
 export default function AdPricePart({price = 0, isSold = true}) {
     return(
         <div className="ad-price-part">
-            <p className={`ad-price-part-title ${isSold ? "ad-price-part-sold" : ""}`}>{price.toString()} $</p>
+            <div className={`ad-price-part-title ${isSold ? "ad-price-part-sold" : ""}`}>{price.toString()} $</div>
             
             {isSold ? 
                 (
