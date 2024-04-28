a4daaff067320c9b4701b0eb67b4d18985fe9299
AchrafGroiez <41470d0a@gmail.com>
2024-04-28 00:29:32 -0400

    Fixed UserProfil, problem with Year, hided attribut that are undefined, remove location and reduced backend query

--- src/services/CustomerInfo.ts
+++ src/services/CustomerInfo.ts
@@ -1,9 +1,5 @@
 import http from "../http-commons";
 
-export const getCustomerInfo = async (link? : String) => {
+export const getCustomerProfil = async (link? : String) => {
     return await http.get(`/user/${link}`);
-}
-
-export const getPublicUserAds = async(link? : String) => {
-    return await http.get(`/user/${link}/public-user-ads`)
 }
