a8715aa572a2d635a1fa36cfc074bcc72dde694b
Messicat <112326931+JF195@users.noreply.github.com>
2024-04-28 15:34:04 -0400

    404 not found page updated

--- src/component/page/NotFound.tsx
+++ src/component/page/NotFound.tsx
@@ -4,10 +4,17 @@ import LoadingIcon from "../part/LoadingIcon";
 export default class NotFound extends Component<{}> {
     public render(): ReactNode {
         return(
-            <div className="main-background">
-                <p>404 Not-Found</p> 
-                <a href="/home">{">"} Go back home {">"}</a>
-                <LoadingIcon/>
+            <div className="main-background d-flex"  style={{width:"90vw"}}>
+                <div style={{width:"70%"}}>
+                    <h1 className="mb-4"><b>404 Not-Found</b></h1> 
+                    <p style={ {marginRight: "2em"} } >
+                        This page does not exist. 
+                        Make sure the link you're using doesn't have a typo, or any other plausible mistake.
+                        
+                    </p> 
+                    <a className="btn bg-primary text-white" href="/">Home</a>
+                </div>
+                <img width={"80%"} src="/img/auth-deco.jpg" alt="" />
             </div>
         );
     }
