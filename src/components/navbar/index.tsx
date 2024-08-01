5127c43c77dc31ae0b885d43d8be9abfd93b9119
AchrafGroiez <41470d0a@gmail.com>
2024-07-31 20:28:43 -0400

    Replaced old mobile link menu with a drawer.

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,11 +1,12 @@
 import { MuiMenu, MuiMenuItem } from '@components/shared/mui-menu';
 import { DESKTOP_VIEW, MOBILE_VIEW, useAppContext } from '@context/AppContext';
+import CloseIcon from '@mui/icons-material/Close';
 import LogoutIcon from '@mui/icons-material/Logout';
 import MenuIcon from '@mui/icons-material/Menu';
 import PersonIcon from "@mui/icons-material/Person";
 import SettingsIcon from '@mui/icons-material/Settings';
 import WebIcon from '@mui/icons-material/Web';
-import { Box, Button, Divider, Fade, IconButton, MenuItem } from '@mui/material';
+import { Box, Button, Divider, Drawer, IconButton, ListItem, ListItemButton, ListItemText, MenuItem } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
 import Avatar from '@mui/material/Avatar';
 import Container from '@mui/material/Container';
@@ -18,7 +19,8 @@ import { Link, useNavigate } from 'react-router-dom';
 import AppNavLink from './components/app-nav-link';
 import Logo from './components/logo';
 import links from "./links.json";
-import CloseIcon from '@mui/icons-material/Close';
+
+const drawerWidth = "100%";
 
 /**
  * 
@@ -55,85 +57,91 @@ export default function Navbar(): ReactElement {
     }
 
     return (
-        <AppBar position="static" enableColorOnDark color="transparent">
-            <Container maxWidth={false}>
-                <Toolbar disableGutters sx={{ display: "flex", justifyContent: "space-between" }}>
-                    {/* <ThemeToggle /> */}
+        <>
+            <AppBar sx={{ bgcolor : "background.paper", zIndex: (theme) => theme.zIndex.drawer + 1 }} position="fixed" enableColorOnDark color="transparent">
+                <Container maxWidth={false}>
+                    <Toolbar disableGutters sx={{ display: "flex", justifyContent: "space-between" }}>
+                        {/* <ThemeToggle /> */}
 
-                    {/* MOBILE SECTION */}
-                    <Stack display={MOBILE_VIEW} direction="row" alignItems="center">
-                        <IconButton onClick={() => setIsMenuDisplayed(!isMenuDisplayed)}>
-                            {isMenuDisplayed ? <CloseIcon sx={{ color: "text.primary" }} /> : <MenuIcon sx={{ color: "text.primary" }} />}
-                        </IconButton>
-                    </Stack>
-                    <Logo />
+                        {/* MOBILE SECTION */}
+                        <Stack display={MOBILE_VIEW} direction="row" alignItems="center">
+                            <IconButton onClick={() => setIsMenuDisplayed(!isMenuDisplayed)}>
+                                {isMenuDisplayed ? <CloseIcon sx={{ color: "text.primary" }} /> : <MenuIcon sx={{ color: "text.primary" }} />}
+                            </IconButton>
+                        </Stack>
+                        <Logo />
 
-                    {/* DESKTOP SECTION */}
-                    <Stack alignItems="center" direction="row" spacing={5} sx={{ display: DESKTOP_VIEW }}>
-                        <Logo isDesktop />
+                        {/* DESKTOP SECTION */}
+                        <Stack alignItems="center" direction="row" spacing={5} sx={{ display: DESKTOP_VIEW }}>
+                            <Logo isDesktop />
 
-                        {links.navbar?.map((link) => (
-                            <AppNavLink key={link.label} {...link} />
-                        ))}
-                    </Stack>
+                            {links.navbar?.map((link) => (
+                                <AppNavLink key={link.label} {...link} />
+                            ))}
+                        </Stack>
 
-                    {/* COMMON SECTION */}
-                    {customerDto ?
-                        (
-                            <MuiMenu menuIcon={
-                                <Avatar src={customerDto?.iconPath}>
-                                    {customerDto?.iconPath ? null : customerDto?.username?.at(0)}
-                                </Avatar>
-                            }>
-                                <Stack component={MenuItem}>
-                                    <Typography textAlign="center" variant="h6">{customerDto?.username ?? "Guest"}</Typography>
-                                </Stack>
-                                <Divider />
+                        {/* COMMON SECTION */}
+                        {customerDto ?
+                            (
+                                <MuiMenu menuIcon={
+                                    <Avatar src={customerDto?.iconPath}>
+                                        {customerDto?.iconPath ? null : customerDto?.username?.at(0)}
+                                    </Avatar>
+                                }>
+                                    <Stack component={MenuItem}>
+                                        <Typography textAlign="center" variant="h6">{customerDto?.username ?? "Guest"}</Typography>
+                                    </Stack>
+                                    <Divider />
 
-                                {DROPDOWN_MENU.map(item => (
+                                    {DROPDOWN_MENU.map(item => (
+                                        <MuiMenuItem
+                                            {...item}
+                                            key={createRandomKey()}
+                                            action={() => navigate(item.path)}
+                                        />
+                                    ))}
+                                    <Divider />
+
                                     <MuiMenuItem
-                                        {...item}
-                                        key={createRandomKey()}
-                                        action={() => navigate(item.path)}
+                                        icon={<LogoutIcon />}
+                                        action={logoutAction}
+                                        label="Logout"
                                     />
-                                ))}
-                                <Divider />
-
-                                <MuiMenuItem
-                                    icon={<LogoutIcon />}
-                                    action={logoutAction}
-                                    label="Logout"
-                                />
-                            </MuiMenu>
-                        ) : (
-                            <Stack direction="row" spacing={1.5}>
-                                <Link to="/login">
-                                    <Button variant="outlined">Sign in</Button>
-                                </Link>
-
-                                <Box sx={{ display: DESKTOP_VIEW }}>
-                                    <Link to="/signup">
-                                        <Button>Get Started</Button>
+                                </MuiMenu>
+                            ) : (
+                                <Stack direction="row" spacing={1.5}>
+                                    <Link to="/login">
+                                        <Button variant="outlined">Sign in</Button>
                                     </Link>
-                                </Box>
-                            </Stack>
-                        )
-                    }
-                </Toolbar>
 
-                <Fade
-                    in={isMenuDisplayed}
-                    unmountOnExit
-                >
-                    <Stack sx={{
-                        display: MOBILE_VIEW
-                    }} justifyContent="center" alignItems="center">
-                        {links.navbar.map((link) => (
-                            <AppNavLink key={createRandomKey()} {...link} />
-                        ))}
-                    </Stack>
-                </Fade>
-            </Container>
-        </AppBar>
+                                    <Box sx={{ display: DESKTOP_VIEW }}>
+                                        <Link to="/signup">
+                                            <Button>Get Started</Button>
+                                        </Link>
+                                    </Box>
+                                </Stack>
+                            )
+                        }
+                    </Toolbar>
+                </Container>
+            </AppBar>
+
+            <Drawer
+                sx={{
+                    display: MOBILE_VIEW,
+                    [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+                }}
+                open={isMenuDisplayed} onClose={() => setIsMenuDisplayed(false)}>
+                <Toolbar />
+
+                <Stack component={ListItem}>
+                    {links.navbar.map(link => (
+                        <ListItemButton>
+                            <ListItemText primary={link.label} />
+                        </ListItemButton>
+                    ))}
+                </Stack>
+            </Drawer>
+        </>
     );
 }
