5d6ad739741f31bfc507d0c488ad3db1d7ed71e9
Messicat <112326931+JF195@users.noreply.github.com>
2024-08-19 16:34:34 -0400

    404 page changed

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,6 +1,6 @@
-import { Stack } from "@mui/material";
+import { DESKTOP_VIEW } from "@context/AppContext";
+import { Box, Stack, Button } from "@mui/material";
 import { Component, ReactNode } from "react";
-import { NavLink } from "react-router-dom";
 
 export default class NotFound extends Component<{}> {
     public render(): ReactNode {
@@ -14,14 +14,15 @@ export default class NotFound extends Component<{}> {
                             <p className="mb-4" style={ {marginRight: "2em"} } >
                                 This page does not exist. 
                                 Make sure the link you're using doesn't have a typo, or any other plausible mistake.
-                                
                             </p>
-                            <NavLink  style={{width:"8em", height:"4em", backgroundColor : "#232751", color : "#FCFBFA", fontSize : "1.25vw", padding : "0.3vw 15px", borderRadius : "10px", textDecoration : "none"}} to="/">Home</NavLink>
+                            <Button href="/">Home</Button>
+                            
                         </div>
                     </div>
-                    <div style={{width:"50%"}} className="d-flex">
-                        <img style={{width:"100%", height:"100%"}} className="float-right" src="/img/auth-deco.jpg" alt="" />
-                    </div> 
+                    <Box style={{width:"50%"}} sx={{ display: DESKTOP_VIEW }}>
+                            <img style={{width:"100%", height:"100%"}} className="float-right" src="/img/auth-deco.jpg" alt="" />
+                    </Box>
+                    
                 </Stack>
             </div>
         );
