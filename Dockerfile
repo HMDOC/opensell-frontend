2d23b384ebc88ecd05472f63c3e8e3e09c5a0567
AchrafGroiez <41470d0a@gmail.com>
2024-10-10 15:08:15 -0400

    Added .dockerignore, changed Dockerfile and added --host option in dev.

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ WORKDIR /home/opensell
 COPY . .
 RUN npm i
 RUN npm run build
-CMD [ "npm", "run", "--host", "preview" ]
+# We run temporarily in dev to be able to change env variables in the docker compose.
+CMD [ "npm", "run", "dev" ]
 EXPOSE 3000
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default ({ mode }: any) => {
-	const env = loadEnv(mode, "./");
-	const PORT: any = env?.VITE_PORT;
+	process.env = loadEnv(mode, "./");
+	let port = Number(process.env.VITE_PORT) || 3000;
 
 	return defineConfig({
 		server: {
-			port: PORT 
+			port,
 		},
-		preview : {
-			port: PORT
+		preview: {
+			port,
 		},
 		plugins: [tsconfigPaths(), react()]
 	})
