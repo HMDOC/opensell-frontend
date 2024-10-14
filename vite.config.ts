a09b9f135a2920c37ebf01de761e23c5f5075710
AchrafGroiez <41470d0a@gmail.com>
2024-10-13 20:53:24 -0400

    Added plugin to run VITE with HTTPS.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
 import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
 export default ({ mode }: any) => {
-	process.env = loadEnv(mode, "./");
-	let port = Number(process.env.VITE_PORT) || 3000;
+  process.env = loadEnv(mode, "./");
+  const port = Number(process.env.VITE_PORT) || 3000;
+  const https = Boolean(process.env.VITE_HTTPS);
 
-	return defineConfig({
-		server: {
-			port,
-		},
-		preview: {
-			port,
-		},
-		plugins: [tsconfigPaths(), react()]
-	})
-}
+  return defineConfig({
+    server: {
+      port,
+    },
+    preview: {
+      port,
+    },
+    plugins: [tsconfigPaths(), react(), https ? basicSsl() : null],
+  });
+};
